3b6f4419aac8fe1b680ec64d98ecc6b5c0654ac0
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-10-20 18:30:19 -0300

    add solution to consultin banking analyses

---
+++ consulting-banking-analysis/src/Analise.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class Analise {
+    Date data;
+    String descricao;
+
+    public Analise(Date date, String descricao) {
+        this.data = date;
+        this.descricao = descricao;
+    }
+}
