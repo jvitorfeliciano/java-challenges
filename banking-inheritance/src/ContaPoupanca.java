4d735f7d71da096400203acae19b89ca0a65891d
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-10-20 15:36:13 -0300

    add solution to banking inheritance

---
+++ banking-inheritance/src/ContaPoupanca.java
@@ -0,0 +1,14 @@
+class ContaPoupanca extends ContaBancaria {
+  private double taxaJuros;
+
+  public ContaPoupanca(int numero, String titular, double saldo, double taxaJuros) {
+    super(numero, titular, saldo);
+    this.taxaJuros = taxaJuros;
+  }
+
+  public void exibirInformacoes() {
+    super.exibirInformacoes();
+    System.out.println("Taxa de juros: " + taxaJuros + "%");
+    
+  }
+}
